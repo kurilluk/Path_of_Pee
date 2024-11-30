13ec5629148afe1ab8ca5110a1a79e1ca7f05d98
Denis Zykov <deniszykov@gmail.com>
2024-11-30 13:32:23 +0100

    fix: move player

--- _tests/denis/scripts/MapSeeding.gd
+++ _tests/denis/scripts/MapSeeding.gd
@@ -15,6 +15,7 @@ var map_width = initial_width
 var map_height = initial_height 
 var map_offset = 4 #Shifts map four rows down for UI
 var rng = RandomNumberGenerator.new()
+var player_coords: Vector2i;
 
 # Tilemap constants
 const BACKGROUND_TILE_ID = 0
@@ -29,7 +30,7 @@ enum Direction { N, NE, E, SE, S, SW, W, NW }
 func _ready():
 	_generate_map()
 	_execute_every_second()
-	move_player(Vector2i(initial_width / 2, initial_height / 2));
+	_place_player(Vector2i(initial_width / 2, initial_height / 2));
 	
 func _execute_every_second():
 	var directions: Array = [Direction.N, Direction.NE, Direction.E, Direction.SE, Direction.S, Direction.SW, Direction.W, Direction.NW]
@@ -46,11 +47,22 @@ func _generate_map():
 	_generate_background()
 	#_cast_shadows(Direction.SW)
 
-func move_player(coords: Vector2i):
+func move_player(relative_coords: Vector2i):
+	var new_player_coords = player_coords + relative_coords;
+	if (!no_obstacles(new_player_coords)):
+		return false;
+	
+	playerTiles.clear();
+	playerTiles.set_cell(new_player_coords, PLAYER_TILE_ID, Vector2i(0, 0), 0)
+	player_coords = new_player_coords;
+	return true;
+
+func _place_player(coords: Vector2i):
 	while (!no_obstacles(coords)):
 		coords += Vector2i(randi() % 2 - 1, randi() % 2 - 1)
 	playerTiles.clear();
 	playerTiles.set_cell(coords, PLAYER_TILE_ID, Vector2i(0, 0), 0)
+	player_coords = coords;
 
 # Checks if tiles are empty or not
 func no_obstacles(coords: Vector2i):
