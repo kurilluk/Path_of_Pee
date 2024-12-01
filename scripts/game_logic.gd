9e042a98ff9a831eeb0d7f7f331ec2767457b029
Lukas Kurilla <kurilluk@email.cz>
2024-12-01 13:37:15 +0100

    fist drink sound and new icon for dink

--- scripts/game_logic.gd
+++ scripts/game_logic.gd
@@ -8,6 +8,8 @@ extends Node2D
 @onready var blader_bar: ProgressBar = %BladerBar
 @onready var start_button: Button = %StartButton
 
+@onready var SFX: AudioStreamPlayer = $SFX
+
 var last_direction = Vector2.ZERO
 var effort : int = 0
 var day_part: int = 0
@@ -77,6 +79,7 @@ func drink():
 	var emount = 100 - hydration_bar.value
 	hydration_bar.value = hydration_bar.max_value
 	blader_bar.value += emount*BLADDER_RATIO
+	SoundManager.play_sound(SFX,SoundManager.SFX_DINK)
 	# TODO add blader by day progression
 #func balance_liquids():
 
@@ -103,7 +106,8 @@ func generate_level_map(level: int = 1) -> Vector2:
 #
 	#return map.place_player();
 	map.generate_map(INITIAL_MAZE_SIZE+(2*level), INITIAL_MAZE_SIZE+(2*level))
-	map.place_items(15,5)
+	map.place_moveable_blocks(level)
+	map.place_items(15,4+(2*level))
 	map.position = Vector2.ZERO
 	map.position = - map.place_player()
 	return Vector2.ZERO #map.place_player()
